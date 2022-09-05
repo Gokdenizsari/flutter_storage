77ec0b16243edd1b1fc876b807320ce8974c096f
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-05 20:10:03 +0300

    Update sherad_preference.dart

--- lib/sherad_preference.dart
+++ lib/sherad_preference.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_storage/model/gender.dart';
 
@@ -10,6 +11,8 @@ class SharePreferencePage extends StatefulWidget {
 
 class _SharePreferencePageState extends State<SharePreferencePage> {
   var _selectedGender = Gender.Women;
+
+  var _selectedColors = [];
 
   @override
   Widget build(BuildContext context) {
@@ -61,6 +64,24 @@ class _SharePreferencePageState extends State<SharePreferencePage> {
                   _selectedGender = selectedGender!;
                 });
               }),*/
+          _buildRadioListTiles("Man", Gender.Man),
+          _buildRadioListTiles("Women", Gender.Women),
+          _buildRadioListTiles("Other", Gender.Other),
+          _buildChecboxListTiles(Colors1.Blue),
+          _buildChecboxListTiles(Colors1.Red),
+          _buildChecboxListTiles(Colors1.Purple),
+          _buildChecboxListTiles(Colors1.Orange),
+          /*   CheckboxListTile(
+              title: Text(describeEnum(Colors1.Purple)),
+              value: _selectedColors.contains(Colors1.Purple),
+              onChanged: (bool? value) {
+                if (value == false) {
+                  _selectedColors.remove(describeEnum(Colors1.Purple));
+                } else {
+                  _selectedColors.add(describeEnum(Colors1.Purple));
+                }
+                setState(() {});
+              })*/
         ],
       ),
     );
@@ -75,6 +96,20 @@ class _SharePreferencePageState extends State<SharePreferencePage> {
           setState(() {
             _selectedGender = selectedGender!;
           });
+        });
+  }
+
+  Widget _buildChecboxListTiles(Colors1 colors1) {
+    return CheckboxListTile(
+        title: Text(describeEnum(colors1)),
+        value: _selectedColors.contains(colors1),
+        onChanged: (bool? value) {
+          if (value == false) {
+            _selectedColors.remove(describeEnum(colors1));
+          } else {
+            _selectedColors.add(describeEnum(colors1));
+          }
+          setState(() {});
         });
   }
 }
